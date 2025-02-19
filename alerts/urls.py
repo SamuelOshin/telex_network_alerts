be2ba25c9c71816c4fc484fa6190f279a70ac22f
SamuelOshin <samuelt.oshin@gmail.com>
2025-02-19 13:13:48 +0100

    feat: rename Telex JSON endpoint to integration.json for clarity

--- alerts/urls.py
+++ alerts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('check/', views.check_network_status, name='check_status'),
     path('configure/', views.configure_webhook, name='configure_webhook'),
-    path('json/', views.handle_telex_json, name='telex_json'),
+    path('integration.json', views.handle_telex_json, name='telex_json'),
 ]
