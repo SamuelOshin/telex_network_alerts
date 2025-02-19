e5da2418a4e4724a449ee6cc8509f51093760a95
SamuelOshin <samuelt.oshin@gmail.com>
2025-02-19 12:02:32 +0100

    feat: update TARGET_URL in settings to point to the production alert check endpoint

--- network_downtime_alert/settings.py
+++ network_downtime_alert/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Load Telex webhook from environment variables
 TELEX_WEBHOOK_URL = os.getenv("TELEX_WEBHOOK_URL", "https://ping.telex.im/v1/webhooks/0195149e-7ff1-7e9d-b08e-8793e1b61746")
-TARGET_URL = getattr(settings, 'TARGET_URL', '192.168.1.1')
+TARGET_URL = getattr(settings, 'TARGET_URL', 'https://telex-network-alerts.onrender.com/alerts/check')
 MIDDLEWARE.insert(1, 'corsheaders.middleware.CorsMiddleware')
 # CORS_ALLOW_ALL_ORIGINS = True  
 
